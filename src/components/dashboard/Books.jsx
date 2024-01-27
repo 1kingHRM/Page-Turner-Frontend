4667cfe410852f35c3f4847aa5f46edb5840f9de
Tinalexman <taiwoluwatobilobafestus@gmail.com>
2024-01-27 11:13:47 +0100

    Finished the genre section completely

--- src/components/dashboard/Books.jsx
+++ src/components/dashboard/Books.jsx
@@ -128,6 +128,145 @@ const Books = () => {
     setSearchText(event.target.value);
   };
 
+  function uploadBook() {}
+
+  const AddModal = () => {
+    return (
+      <div className="flex flex-col">
+        <p className="text-2xl text-tertiary text-center pb-5">Add New Book?</p>
+        <TextInput
+          title="Book Title"
+          placeholder="Enter Book Title"
+          color="brown.6"
+        />
+        <div className="h-5" />
+        <Select
+          placeholder="Select Genre"
+          data={genres.map((genre, i) => {
+            return genre.name;
+          })}
+          // value={credentials.category}
+          // onChange={(e) => {
+          //   if (e !== null) {
+          //     setCredentials({ ...credentials, category: e });
+          //   }
+          // }}
+        />
+        <div className="h-5" />
+        <TextInput
+          title="Book Author"
+          placeholder="Enter Book Author"
+          color="brown.6"
+        />
+        <div className="h-5" />
+
+        <Textarea
+          title="Book Decription"
+          placeholder="Enter Book Description"
+          color="brown.6"
+        />
+        <div className="h-10" />
+
+        <button
+          onClick={uploadBook}
+          className="w-full rounded-lg bg-primary text-white font-medium py-2"
+        >
+          Upload
+        </button>
+      </div>
+    );
+  };
+
+  const DeleteModal = ({ book, onCancel }) => {
+    return (
+      <div className="flex flex-col">
+        <p className="text-2xl text-tertiary text-center mt-5">
+          Are you sure you want to delete this book?
+        </p>
+        <div className="flex items-center justify-around w-full mt-5">
+          <Image src={book.image} alt="" className="" />
+          <div className="flex flex-col w-[50%]">
+            <p>{book.name}</p>
+            <p>By {book.author}</p>
+          </div>
+        </div>
+        <div className="flex justify-around py-5">
+          <button
+            className="w-[40%] border-[1.5px] border-black py-2 rounded-lg"
+            onClick={onCancel}
+          >
+            Cancel
+          </button>
+          <button
+            className="w-[40%] bg-red-700 py-2 rounded-lg text-white font-medium"
+            onClick={onCancel}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+  const EditModal = ({ book, onEdit }) => {
+    return (
+      <div className="flex flex-col">
+        <p className="text-2xl text-tertiary text-center pb-5">Edit Book</p>
+        <TextInput
+          title="Book Title"
+          placeholder="Enter Book Title"
+          color="brown.6"
+          value={book.name}
+        />
+        <div className="h-5" />
+        <Select
+          placeholder="Select Genre"
+          data={[
+            "Handmade Goods",
+            "Jewelry",
+            "Home Decor",
+            "Clothing",
+            "Arts",
+            "Vintage Items",
+            "Photography",
+            "Cosmetics",
+            "Toys",
+          ]}
+          value={book.genre}
+          // value={credentials.category}
+          // onChange={(e) => {
+          //   if (e !== null) {
+          //     setCredentials({ ...credentials, category: e });
+          //   }
+          // }}
+        />
+        <div className="h-5" />
+        <TextInput
+          title="Book Author"
+          placeholder="Enter Book Author"
+          color="brown.6"
+          value={book.author}
+        />
+        <div className="h-5" />
+
+        <Textarea
+          title="Book Decription"
+          placeholder="Enter Book Description"
+          color="brown.6"
+          value={book.description}
+        />
+        <div className="h-10" />
+
+        <button
+          onClick={onEdit}
+          className="w-full rounded-lg bg-primary text-white font-medium py-2"
+        >
+          Upload
+        </button>
+      </div>
+    );
+  };
+
   return (
     <>
       <ToastContainer
@@ -166,64 +305,61 @@ const Books = () => {
               </button>
             </div>
           </div>
-          {!loading && books.length > 0 && (
-            <>
-              <div className="my-20 flex flex-wrap gap-10">
-                {books.map((book, i) => {
-                  return (
-                    <div
-                      key={i}
-                      className="bg-white flex flex-col shadow-lg rounded-lg w-[250px] h-[250px]"
-                    >
-                      {/* <Image
-                src={book.image}
-                alt=""
-                className="w-full h-[120px] object-contain pt-5"
-              /> */}
 
-                      <div className="w-full h-[120px] pt-5" />
-                      <div className="px-2 flex flex-col">
-                        <p className="font-medium text-xl text-center text-slate-950">
-                          {book.title}
-                        </p>
-                        <p className="text-md text-center text-slate-950">
-                          By: {book.author}
-                        </p>
-                        <div className="flex gap-5 justify-around items-center text-tertiary my-5">
-                          <div
-                            className="py-2 w-full gap-2 items-center text-white justify-center flex rounded-lg bg-primary cursor-pointer"
-                            onClick={() => openEdit(book)}
-                          >
-                            Edit
-                            <MdEdit size={20} />
-                          </div>
-                          <div
-                            onClick={() => openDelete(book)}
-                            className="py-2  w-full gap-2 items-center text-white justify-center flex rounded-lg bg-primary cursor-pointer"
-                          >
-                            Delete
-                            <MdDelete size={20} />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
-              <Modal
-                opened={opened}
-                onClose={() => setOpened(false)}
-                color="brown.6"
-              >
-                {flag === 0 ? (
+          <Modal opened={opened} onClose={close} color="brown.6">
+            <AddModal />
+            {/* {flag === 0 ? (
                   <EditModal book={selectedBook} onEdit={close} />
                 ) : flag === 1 ? (
                   <DeleteModal book={selectedBook} onCancel={close} />
                 ) : (
                   <AddModal onUpload={close} genres={genres} />
-                )}
-              </Modal>
-            </>
+                )} */}
+          </Modal>
+
+          {!loading && books.length > 0 && (
+            <div className="my-20 flex flex-wrap gap-10">
+              {books.map((book, i) => {
+                return (
+                  <div
+                    key={i}
+                    className="bg-white flex flex-col shadow-lg rounded-lg w-[250px] h-[250px]"
+                  >
+                    {/* <Image
+                src={book.image}
+                alt=""
+                className="w-full h-[120px] object-contain pt-5"
+              /> */}
+
+                    <div className="w-full h-[120px] pt-5" />
+                    <div className="px-2 flex flex-col">
+                      <p className="font-medium text-xl text-center text-slate-950">
+                        {book.title}
+                      </p>
+                      <p className="text-md text-center text-slate-950">
+                        By: {book.author}
+                      </p>
+                      <div className="flex gap-5 justify-around items-center text-tertiary my-5">
+                        <div
+                          className="py-2 w-full gap-2 items-center text-white justify-center flex rounded-lg bg-primary cursor-pointer"
+                          onClick={() => openEdit(book)}
+                        >
+                          Edit
+                          <MdEdit size={20} />
+                        </div>
+                        <div
+                          onClick={() => openDelete(book)}
+                          className="py-2  w-full gap-2 items-center text-white justify-center flex rounded-lg bg-primary cursor-pointer"
+                        >
+                          Delete
+                          <MdDelete size={20} />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           )}
         </div>
       )}
@@ -243,149 +379,4 @@ const Books = () => {
   );
 };
 
-const AddModal = ({ genres, onUpload }) => {
-  return (
-    <div className="flex flex-col">
-      <p className="text-2xl text-tertiary text-center pb-5">Add New Book?</p>
-      <TextInput
-        title="Book Title"
-        placeholder="Enter Book Title"
-        color="brown.6"
-      />
-      <div className="h-5" />
-      <Select
-        placeholder="Select Genre"
-        data={genres.map((genre, i) => {
-          return genre.name;
-        })}
-        // value={credentials.category}
-        // onChange={(e) => {
-        //   if (e !== null) {
-        //     setCredentials({ ...credentials, category: e });
-        //   }
-        // }}
-      />
-      <div className="h-5" />
-      <TextInput
-        title="Book Author"
-        placeholder="Enter Book Author"
-        color="brown.6"
-      />
-      <div className="h-5" />
-
-      <Textarea
-        title="Book Decription"
-        placeholder="Enter Book Description"
-        color="brown.6"
-      />
-      <div className="h-10" />
-
-      <button
-        onClick={onUpload}
-        className="w-full rounded-lg bg-primary text-white font-medium py-2"
-      >
-        Upload
-      </button>
-    </div>
-  );
-};
-
-const DeleteModal = ({ book, onCancel }) => {
-  return (
-    <div className="flex flex-col">
-      <p className="text-2xl text-tertiary text-center mt-5">
-        Are you sure you want to delete this book?
-      </p>
-      <div className="flex items-center justify-around w-full mt-5">
-        <Image src={book.image} alt="" className="" />
-        <div className="flex flex-col w-[50%]">
-          <p>{book.name}</p>
-          <p>By {book.author}</p>
-        </div>
-      </div>
-      <div className="flex justify-around py-5">
-        <button
-          className="w-[40%] border-[1.5px] border-black py-2 rounded-lg"
-          onClick={onCancel}
-        >
-          Cancel
-        </button>
-        <button
-          className="w-[40%] bg-red-700 py-2 rounded-lg text-white font-medium"
-          onClick={onCancel}
-        >
-          Delete
-        </button>
-      </div>
-    </div>
-  );
-};
-
-const EditModal = ({ book, onEdit }) => {
-  return (
-    <div className="flex flex-col">
-      <p className="text-2xl text-tertiary text-center pb-5">Edit Book</p>
-      <TextInput
-        title="Book Title"
-        placeholder="Enter Book Title"
-        color="brown.6"
-        value={book.name}
-      />
-      <div className="h-5" />
-      <Select
-        placeholder="Select Genre"
-        data={[
-          "Handmade Goods",
-          "Jewelry",
-          "Home Decor",
-          "Clothing",
-          "Arts",
-          "Vintage Items",
-          "Photography",
-          "Cosmetics",
-          "Toys",
-        ]}
-        value={book.genre}
-        // value={credentials.category}
-        // onChange={(e) => {
-        //   if (e !== null) {
-        //     setCredentials({ ...credentials, category: e });
-        //   }
-        // }}
-      />
-      <div className="h-5" />
-      <TextInput
-        title="Book Author"
-        placeholder="Enter Book Author"
-        color="brown.6"
-        value={book.author}
-      />
-      <div className="h-5" />
-
-      <Textarea
-        title="Book Decription"
-        placeholder="Enter Book Description"
-        color="brown.6"
-        value={book.description}
-      />
-      <div className="h-10" />
-
-      <button
-        onClick={onEdit}
-        className="w-full rounded-lg bg-primary text-white font-medium py-2"
-      >
-        Upload
-      </button>
-    </div>
-  );
-};
-
 export default Books;
-
-const model = {
-  name: "Name",
-  image: "Image",
-  author: "Author",
-  description: "Description",
-  genre: "Genre",
-};
--- src/components/dashboard/Genres.jsx
+++ src/components/dashboard/Genres.jsx
@@ -14,11 +14,23 @@ import "react-toastify/dist/ReactToastify.css";
 
 const Genres = () => {
   const [loading, setLoading] = useState(false);
+  const [uploading, setUploading] = useState(false);
+
   const [genres, setGenres] = useState([]);
+  const [searchedGenres, setSearchedGenres] = useState([]);
+
   const [open, setOpen] = useState(false);
+  const [flag, setFlag] = useState(-1);
+
+  const [genreName, setGenreName] = useState("");
+  const [searchText, setSearchText] = useState("");
+
+  const [reloadFlag, setReloadFlag] = useState(false);
+
+  const [currentGenre, setCurrentGenre] = useState({});
 
   useEffect(() => {
-    if (genres.length === 0) {
+    if (genres.length === 0 || reloadFlag) {
       setLoading(true);
       let userData = window.localStorage.getItem("page-turner");
       userData = JSON.parse(userData);
@@ -26,15 +38,19 @@ const Genres = () => {
       axios({
         method: "GET",
         url: `${baseUrl}/genres`,
-      }).then((res) => {
-        setGenres(res.data.payload)
-        setLoading(false);
-      }).catch((err) => {
-        toast.error("Could not fetch the genres from the server");
-        setLoading(false);
-      });
+      })
+        .then((res) => {
+          setGenres(res.data.payload);
+          setLoading(false);
+          setReloadFlag(false);
+        })
+        .catch((err) => {
+          toast.error("Could not fetch the genres from the server");
+          setLoading(false);
+          setReloadFlag(false);
+        });
     }
-  }, [])
+  }, [reloadFlag]);
 
   function splitWords(text) {
     let splits = text.split(" ");
@@ -48,9 +64,195 @@ const Genres = () => {
     return value;
   }
 
+  const uploadGenre = (edit = false) => {
+    setUploading(true);
+    let userData = window.localStorage.getItem("page-turner");
+    userData = JSON.parse(userData);
+
+    let dto = {
+      name: genreName,
+    };
+
+    if (edit) {
+      dto._id = currentGenre._id;
+    }
+
+    axios({
+      method: edit ? "PUT" : "POST",
+      url: `${baseUrl}/genres/${edit ? "edit" : "add"}`,
+      data: dto,
+      headers: { Authorization: `Bearer ${userData.token}` },
+    })
+      .then((res) => {
+        toast.success(`${edit ? "Edited" : "Created"} the genre successfully`);
+        cancel();
+      })
+      .catch((err) => {
+        toast.error(`Could not ${edit ? "edit" : "add"} the genre`);
+        setUploading(false);
+      });
+  };
+
+  const deleteGenre = () => {
+    setUploading(true);
+    let userData = window.localStorage.getItem("page-turner");
+    userData = JSON.parse(userData);
+
+    axios({
+      method: "DELETE",
+      url: `${baseUrl}/genres/delete/${currentGenre._id}`,
+      headers: { Authorization: `Bearer ${userData.token}` },
+    })
+      .then((res) => {
+        toast.success(`Deleted the genre successfully`);
+        cancel();
+      })
+      .catch((err) => {
+        toast.error(`Could not delete the genre`);
+        setUploading(false);
+      });
+  };
+
+  function onSearch(text) {
+    if (text.length === 0) {
+      setSearchedGenres([]);
+      return;
+    }
+
+    const lowerText = text.toLowerCase();
+    const result = genres.filter((genre) =>
+      genre.name.toLowerCase().startsWith(lowerText)
+    );
+
+    setSearchedGenres(result);
+  }
+
+  function cancel() {
+    setGenreName("");
+    setUploading(false);
+    setReloadFlag(true);
+    setOpen(false);
+  }
+
   const openAdd = () => {
     setOpen(true);
-  }
+    setFlag(0);
+  };
+
+  const openEdit = (genre) => {
+    setOpen(true);
+    setFlag(1);
+    setGenreName(genre.name);
+    setCurrentGenre(genre);
+  };
+
+  const openDelete = (genre) => {
+    setOpen(true);
+    setFlag(2);
+    setCurrentGenre(genre);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      onSearch();
+    }
+  };
+
+  const onTextChange = (event) => {
+    setSearchText(event.target.value);
+    onSearch(event.target.value);
+  };
+
+  const AddModalComponent = () => {
+    return (
+      <>
+        {!uploading && (
+          <div className="flex flex-col">
+            <p className="text-2xl text-tertiary text-center pb-5">Add Genre</p>
+
+            <TextInput
+              title="Genre Name"
+              value={genreName}
+              onChange={(e) => setGenreName(e.target.value)}
+              placeholder="Enter Genre Name"
+            />
+
+            <button
+              onClick={uploadGenre}
+              className="w-full mt-5 rounded-lg bg-primary text-white font-medium py-2"
+            >
+              Upload
+            </button>
+          </div>
+        )}
+
+        {uploading && (
+          <div className="flex flex-col w-full h-32 items-center justify-center">
+            <Loader color="brown.6" />
+          </div>
+        )}
+      </>
+    );
+  };
+
+  const EditModalComponent = () => {
+    return (
+      <>
+        {!uploading && (
+          <div className="flex flex-col">
+            <p className="text-2xl text-tertiary text-center pb-5">
+              Edit Genre
+            </p>
+
+            <TextInput
+              title="Genre Name"
+              value={genreName}
+              onChange={(e) => setGenreName(e.target.value)}
+              placeholder="Enter Genre Name"
+            />
+
+            <button
+              onClick={() => uploadGenre(true)}
+              className="w-full mt-5 rounded-lg bg-primary text-white font-medium py-2"
+            >
+              Update
+            </button>
+          </div>
+        )}
+
+        {uploading && (
+          <div className="flex flex-col w-full h-32 items-center justify-center">
+            <Loader color="brown.6" />
+          </div>
+        )}
+      </>
+    );
+  };
+
+  const DeleteModalComponent = () => {
+    return (
+      <div className="flex flex-col">
+        <p className="text-2xl text-tertiary text-center mt-5">
+          Are you sure you want to delete the{" "}
+          <span className="font-[600]">{currentGenre.name}</span> genre?
+        </p>
+        <div className="flex justify-around py-5">
+          <button
+            className="w-[40%] border-[1.5px] border-black py-2 rounded-lg"
+            onClick={cancel}
+          >
+            Cancel
+          </button>
+          <button
+            className="w-[40%] bg-red-700 py-2 rounded-lg text-white font-medium"
+            onClick={deleteGenre}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    );
+  };
 
   return (
     <>
@@ -62,8 +264,8 @@ const Genres = () => {
         rtl={false}
         theme="colored"
       />
-      {
-        !loading && <div className="w-full flex flex-col h-auto px-10">
+      {!loading && (
+        <div className="w-full flex flex-col h-auto px-10">
           <div className="flex flex-row justify-between items-center">
             <div>
               <p className="text-2xl text-slate-950 mt-5 font-medium">Genres</p>
@@ -73,7 +275,14 @@ const Genres = () => {
             </div>
 
             <div className="flex items-center gap-10">
-
+              <input
+                type="search"
+                placeholder="Search Genre"
+                value={searchText}
+                onKeyDown={handleKeyDown}
+                onChange={onTextChange}
+                className="focus:outline-none py-2 px-3 border border-tertiary1 rounded-lg w-[300px] text-tertiary bg-offWhite"
+              />
               <button
                 onClick={openAdd}
                 className="py-2 px-4 bg-primary rounded-lg flex items-center gap-2 shadow-lg text-white"
@@ -83,50 +292,94 @@ const Genres = () => {
               </button>
             </div>
           </div>
-          <div className="my-20 flex flex-wrap gap-10">
-            {genres.map((genre, i) => {
-              return (
-                <div
-                  key={i}
-                  className="bg-white flex flex-col shadow-lg w-[200px] px-5 rounded-lg"
-                >
-                  <p className="text-center w-full pt-5 text-tertiary text-2xl">{genre.name}</p>
 
-                  <div className="flex gap-5 justify-around items-center text-tertiary my-5">
-                    <div
-                      className="py-2 w-full gap-2 items-center text-white justify-center flex rounded-lg bg-primary cursor-pointer"
-                      onClick={() => openEdit(book)}
-                    >
-                      <MdEdit size={20} />
-                    </div>
-                    <div
-                      onClick={() => openDelete(book)}
-                      className="py-2  w-full gap-2 items-center text-white justify-center flex rounded-lg bg-primary cursor-pointer"
-                    >
-                      <MdDelete size={20} />
+          <Modal
+            opened={open}
+            onClose={() => {
+              setOpen(false);
+              setGenreName("");
+              setUploading(false);
+            }}
+            color="brown.6"
+          >
+            {flag === 0 && <AddModalComponent />}
+            {flag === 1 && <EditModalComponent />}
+            {flag == 2 && <DeleteModalComponent />}
+          </Modal>
+
+          {searchedGenres.length === 0 && (
+            <div className="my-20 flex flex-wrap gap-10">
+              {genres.map((genre, i) => {
+                return (
+                  <div
+                    key={i}
+                    className="bg-white flex flex-col shadow-lg w-[200px] px-5 rounded-lg"
+                  >
+                    <p className="text-center w-full pt-5 text-tertiary text-2xl">
+                      {genre.name}
+                    </p>
+
+                    <div className="flex gap-5 justify-around items-center text-tertiary my-5">
+                      <div
+                        className="py-2 w-full gap-2 items-center text-white justify-center flex rounded-lg bg-primary cursor-pointer"
+                        onClick={() => openEdit(genre)}
+                      >
+                        <MdEdit size={20} />
+                      </div>
+                      <div
+                        onClick={() => openDelete(genre)}
+                        className="py-2  w-full gap-2 items-center text-white justify-center flex rounded-lg bg-primary cursor-pointer"
+                      >
+                        <MdDelete size={20} />
+                      </div>
                     </div>
                   </div>
+                );
+              })}
+            </div>
+          )}
 
+          {searchedGenres.length > 0 && (
+            <div className="my-20 flex flex-wrap gap-10">
+              {searchedGenres.map((genre, i) => {
+                return (
+                  <div
+                    key={i}
+                    className="bg-white flex flex-col shadow-lg w-[200px] px-5 rounded-lg"
+                  >
+                    <p className="text-center w-full pt-5 text-tertiary text-2xl">
+                      {genre.name}
+                    </p>
 
-                </div>
-              );
-            })}
-          </div>
-
+                    <div className="flex gap-5 justify-around items-center text-tertiary my-5">
+                      <div
+                        className="py-2 w-full gap-2 items-center text-white justify-center flex rounded-lg bg-primary cursor-pointer"
+                        onClick={() => openEdit(genre)}
+                      >
+                        <MdEdit size={20} />
+                      </div>
+                      <div
+                        onClick={() => openDelete(genre)}
+                        className="py-2  w-full gap-2 items-center text-white justify-center flex rounded-lg bg-primary cursor-pointer"
+                      >
+                        <MdDelete size={20} />
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          )}
         </div>
-      }
+      )}
 
-
-      {
-        loading && <div className="h-44 w-full flex flex-col justify-center items-center ">
+      {loading && (
+        <div className="h-44 w-full flex flex-col justify-center items-center ">
           <Loader color="brown.6" />
         </div>
-      }
+      )}
     </>
   );
 };
 
-
-
 export default Genres;
-
